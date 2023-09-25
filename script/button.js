a1209e383fae6a4915ff362b4a240196f69264e7
erickpati10 <pat21018@byui.edu>
2023-09-24 21:03:28 -0600

    updates

--- script/button.js
+++ script/button.js
@@ -1,6 +1,21 @@
-const menuToggle = document.querySelector(".menu-toggle");
+// const menuToggle = document.querySelector(".menu-toggle");
+// const navMenu = document.querySelector(".nav-menu");
+
+// menuToggle.addEventListener("click", () => {
+//   navMenu.classList.toggle("active");
+// });
+
+const hamburger = document.querySelector(".menu-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
-menuToggle.addEventListener("click", () => {
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
